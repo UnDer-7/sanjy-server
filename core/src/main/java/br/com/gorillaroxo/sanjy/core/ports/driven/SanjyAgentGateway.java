d1087f9d4db26b97be7150b52d2f2cb704c25935
UnDer-7 <mateus7532@gmail.com>
2025-10-17 00:22:39 -0300

    removing chat

--- core/src/main/java/br/com/gorillaroxo/sanjy/core/ports/driven/SanjyAgentGateway.java
+++
@@ -1,7 +0,0 @@
-package br.com.gorillaroxo.sanjy.core.ports.driven;
-
-public interface SanjyAgentGateway {
-
-    String ask(String inputMessage, Long dietPlanId);
-
-}
--- core/src/main/java/br/com/gorillaroxo/sanjy/core/ports/driver/AgentSanjyUseCase.java
+++
@@ -1,7 +0,0 @@
-package br.com.gorillaroxo.sanjy.core.ports.driver;
-
-public interface AgentSanjyUseCase {
-
-    String execute(final String inputMessage);
-
-}
