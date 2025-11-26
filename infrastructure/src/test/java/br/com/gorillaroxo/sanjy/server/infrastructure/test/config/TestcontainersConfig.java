0980fd1534ca58ec064a01b8558f1f82e8f2ac01
UnDer-7 <mateus7532@gmail.com>
2025-11-26 14:02:02 -0300

    database graalvm test

--- infrastructure/src/test/java/br/com/gorillaroxo/sanjy/server/infrastructure/test/config/TestcontainersConfig.java
+++ infrastructure/src/test/java/br/com/gorillaroxo/sanjy/server/infrastructure/test/config/TestcontainersConfig.java
@@ -1,5 +1,7 @@
 package br.com.gorillaroxo.sanjy.server.infrastructure.test.config;
 
+import org.springframework.boot.autoconfigure.flyway.FlywayConnectionDetails;
+import org.springframework.boot.autoconfigure.jdbc.JdbcConnectionDetails;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.annotation.Bean;
@@ -11,6 +13,57 @@ public class TestcontainersConfig {
     @Bean
     @ServiceConnection
     public PostgreSQLContainer<?> postgresContainer() {
-        return new PostgreSQLContainer<>("postgres:17-alpine");
+        PostgreSQLContainer<?> container = new PostgreSQLContainer<>("postgres:17-alpine");
+        container.start();
+        return container;
     }
+
+    /**
+     * Provides JdbcConnectionDetails bean for GraalVM native image tests.
+     * This is necessary because @ServiceConnection doesn't work in AOT mode.
+     */
+    @Bean
+    public JdbcConnectionDetails jdbcConnectionDetails(PostgreSQLContainer<?> container) {
+        return new JdbcConnectionDetails() {
+            @Override
+            public String getJdbcUrl() {
+                return container.getJdbcUrl();
+            }
+
+            @Override
+            public String getUsername() {
+                return container.getUsername();
+            }
+
+            @Override
+            public String getPassword() {
+                return container.getPassword();
+            }
+        };
+    }
+
+    /**
+     * Provides JdbcConnectionDetails bean for GraalVM native image tests.
+     * This is necessary because @ServiceConnection doesn't work in AOT mode.
+     */
+    @Bean
+    public FlywayConnectionDetails flywayConnectionDetails(final PostgreSQLContainer<?> container) {
+        return new FlywayConnectionDetails() {
+            @Override
+            public String getJdbcUrl() {
+                return container.getJdbcUrl();
+            }
+
+            @Override
+            public String getUsername() {
+                return container.getUsername();
+            }
+
+            @Override
+            public String getPassword() {
+                return container.getPassword();
+            }
+        };
+    }
+
 }
