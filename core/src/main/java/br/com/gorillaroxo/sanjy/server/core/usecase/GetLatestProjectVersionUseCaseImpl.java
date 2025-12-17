f700a003ccb2da13593c60626d4ab2aee65074e3
UnDer-7 <mateus7532@gmail.com>
2025-12-16 21:37:04 -0300

    fix feing client

--- core/src/main/java/br/com/gorillaroxo/sanjy/server/core/usecase/GetLatestProjectVersionUseCaseImpl.java
+++ core/src/main/java/br/com/gorillaroxo/sanjy/server/core/usecase/GetLatestProjectVersionUseCaseImpl.java
@@ -1,10 +1,12 @@
 package br.com.gorillaroxo.sanjy.server.core.usecase;
 
+import br.com.gorillaroxo.sanjy.server.core.domain.LogField;
 import br.com.gorillaroxo.sanjy.server.core.ports.driven.GitHubGateway;
 import br.com.gorillaroxo.sanjy.server.core.ports.driven.SanjyServerProps;
 import br.com.gorillaroxo.sanjy.server.core.ports.driver.GetLatestProjectVersionUseCase;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import net.logstash.logback.argument.StructuredArguments;
 import org.springframework.stereotype.Service;
 
 @Slf4j
@@ -17,7 +19,18 @@ class GetLatestProjectVersionUseCaseImpl implements GetLatestProjectVersionUseCa
 
     @Override
     public String execute() {
-        return gitHubGateway.getLatestRelease(sanjyServerProps.application().name()).tagName();
+        log.info(
+            LogField.Placeholders.ONE.getPlaceholder(),
+            StructuredArguments.kv(LogField.MSG.label(), "Getting project latest release version"));
+
+        final String latestVersion = gitHubGateway.getLatestRelease(sanjyServerProps.application().name()).tagName();
+
+        log.info(
+            LogField.Placeholders.TWO.getPlaceholder(),
+            StructuredArguments.kv(LogField.MSG.label(), "Successfully got project latest release version"),
+            StructuredArguments.kv(LogField.PROJECT_LATEST_VERSION.label(), latestVersion));
+
+        return latestVersion;
     }
 
 }
--- infrastructure/src/main/java/br/com/gorillaroxo/sanjy/server/infrastructure/client/rest/github/GitHubReleaseFeignClient.java
+++ infrastructure/src/main/java/br/com/gorillaroxo/sanjy/server/infrastructure/client/rest/github/GitHubReleaseFeignClient.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 @FeignClient(
-    value = "br.com.gorillaroxo.sanjy.server.infrastructure.client.rest.github.GitHubReposFeignClient",
+    value = "GitHubReposFeignClient",
     url = "${sanjy-server.external-http-clients.github.url}",
     path = "/repos/UnDer-7"
 )
